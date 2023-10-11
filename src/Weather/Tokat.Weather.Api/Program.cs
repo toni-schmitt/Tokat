090dbac93d45cf1eca9cf58a2914ea67419d0b4e
Toni Schmitt <toni.schmitt@mmmake.com>
2023-10-11 16:01:27 +0200

    style: add empty line :)

--- src/Weather/Tokat.Weather.Api/Program.cs
+++ src/Weather/Tokat.Weather.Api/Program.cs
@@ -22,6 +22,7 @@ builder.Services.AddSingleton(
 
 builder.Services.AddSingleton<IWeatherService, OpenWeatherMapService>();
 builder.Services.AddSingleton<IWeatherService, WeatherApiService>();
+
 builder.Services.AddHttpClient();
 builder.Services.AddAuthorization();
 builder.Services.AddEndpointsApiExplorer();
